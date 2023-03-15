7a71b351e595ff4a6f45670413a71e2081ca86b0
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-15 17:17:55 +0000

    Update fastq-dump.sh

--- SRA/fastq-dump.sh
+++ SRA/fastq-dump.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # ----------------------------------------------------------
-# Script to convert SRA files to fastq files
+# Script to convert SRA files to fastq files. Testing on PRJNA836755 
 # melodyjparker14@gmail.com - Mar 23
 # ----------------------------------------------------------
 
 #SBATCH -A lindgren.prj
 #SBATCH -p short
-#SBATCH -c 5
+#SBATCH -c 6
 #SBATCH -J fastq-dump
 #SBATCH -o logs/fastq-dump.out
 #SBATCH -e logs/fastq-dump.err
-#SBATCH -a 1-15
+#SBATCH -a 1-8
 
 #  Parallel environment settings 
 #  For more information on these please see the documentation 
@@ -29,7 +29,8 @@ echo "Started at: "`date`
 echo "##########################################################"
 
 
-PROJECT=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' prja_list.txt)
+# PROJECT=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' prja_list.txt)
+READ=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' PRJNA836755_SraAccList.txt)
 
 IN=sra_files  # $1
 OUT=fqd_raw_reads  # $2
@@ -38,8 +39,6 @@ OUT=fqd_raw_reads  # $2
 module load SRA-Toolkit/3.0.0-centos_linux64
 module load parallel/20210722-GCCcore-11.2.0
 
-cd "$PROJECT"
-
 # Create output directory
 if [ ! -p "$OUT" ]
 then
@@ -47,7 +46,7 @@ then
 fi
 
 # Convert SRA files to fastq files
-cat "$PROJECT"_SraAccList.txt | parallel fastq-dump "$IN"/{} --outdir "$OUT" --skip-technical --split-3 --origfmt --gzip
+fastq-dump "$IN"/"$READ" --outdir "$OUT" --skip-technical --split-3 --origfmt --gzip
 
 
 echo "###########################################################"
