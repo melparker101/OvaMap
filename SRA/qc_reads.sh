4068e33890d56a72ff143a13c0ba9c2b269f2308
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-21 16:47:47 +0000

    Create qc_reads.sh

---
+++ SRA/qc_reads.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# ----------------------------------------------------------
+# Script to create perform quality control checks on raw reads
+# This script is to be run from the project directory, e.g. PRJNA766716
+# First argument should be raw_reads dir, second should be the output qc dir
+# melodyjparker14@gmail.com - Dec 22
+# ----------------------------------------------------------
+
+#SBATCH -A lindgren.prj
+#SBATCH -p short
+#SBATCH -c 4
+#SBATCH -J qc_raw_reads
+#SBATCH -o output.out
+#SBATCH -e error.err
+#SBATCH -a 1-15
+
+#  Parallel environment settings 
+#  For more information on these please see the documentation 
+#  Allowed parameters: 
+#   -c, --cpus-per-task 
+#   -N, --nodes 
+#   -n, --ntasks 
+
+echo "########################################################"
+echo "Slurm Job ID: $SLURM_JOB_ID" 
+echo "Run on host: "`hostname` 
+echo "Operating system: "`uname -s` 
+echo "Username: "`whoami` 
+echo "Started at: "`date` 
+echo "##########################################################"
+
+
+IN=$1  # raw_reads
+OUT=$2  # qc_raw_results
+
+if [[ ! -f index.txt ]]; then
+  for f in *_R1_001.fastq.gz; do
+    echo "${f%_R1_001.fastq.gz}" > index.txt
+  done
+fi
+
+# The input file is the name on the task_id'th line of the index list
+INPUT_FILE=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' $IN/index.txt)
+
+module load FastQC/0.11.9-Java-11
+
+echo $INPUT_FILE
+
+fastqc $IN/"$INPUT_FILE"_R1_001.fastq.gz -o $OUT &
+fastqc $IN/"$INPUT_FILE"_R2_001.fastq.gz -o $OUT &
+wait
+
+
+echo "###########################################################"
+echo "Finished at: "`date`
+echo "###########################################################"
+exit 0
