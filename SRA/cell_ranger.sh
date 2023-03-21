4485b2283f8031db3292ef56d1475e43cbd7cf5b
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-21 11:00:43 +0000

    Update cell_ranger.sh

--- SRA/cell_ranger.sh
+++ SRA/cell_ranger.sh
@@ -103,6 +103,9 @@ for f in *S1_L004_R1_001.fastq; do  cat $f | head -1 ; done
 # Do the same for R2 - these should have length >28, e.g. 150
 for f in *S1_L004_R2_001.fastq; do  cat $f | head -1 ; done
 
+# Page for length of R1 and R2
+# https://www.10xgenomics.com/support/single-cell-gene-expression/documentation/steps/sequencing/sequencing-requirements-for-single-cell-3
+
 # --sample $READ excluded as we want to use all (S1)
 # Run cellranger to align and quantify
 cellranger count --id run_count_"$PROJECT" --transcriptome ../"$REF"/refdata-gex-GRCh38-2020-A.tar.gz \
