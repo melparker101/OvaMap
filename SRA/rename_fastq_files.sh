b50e33bbb7d4d0403e9069f9d130129ba4440a42
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-27 10:39:15 +0100

    Update rename_fastq_files.sh

--- SRA/rename_fastq_files.sh
+++ SRA/rename_fastq_files.sh
@@ -10,6 +10,10 @@ P=$1  # Insert project accession as first argument
 
 cd "$P"/raw_reads
 
+##########################
+# For reads split in 2 (lengths 28 and ~150)
+##########################
+
 while read p; do
     lane=$(zcat "$p"_1.fastq.gz | awk -F: '{print $4}' | head -1)
     length=$(zcat "$p"_1.fastq.gz | awk -F'[=\" "]' '{print $4}' | head -1)
@@ -24,7 +28,10 @@ while read p; do
     fi
 done <../"$P"_SraAccList.txt
 
-# Rename the reads that split into three
+##########################
+# For reads split in 3 (lengths 8,28,~150)
+##########################
+
 while read p; do
   if [[ -f "$p"_3.fastq.gz ]]; then
   lane=$(zcat "$p"_1.fastq.gz | awk -F: '{print $4}' | head -1)
@@ -42,3 +49,26 @@ while read p; do
     fi
   fi
 done <../"$P"_SraAccList.txt
+
+##########################
+# For reads split in 2 (150,150)
+##########################
+
+while read p; do
+  if [[ -f "$p"_3.fastq.gz ]]; then
+  lane=$(zcat "$p"_1.fastq.gz | awk -F: '{print $4}' | head -1)
+  length1=$(zcat "$p"_1.fastq.gz | awk -F'[=\" "]' '{print $4}' | head -1)
+  length2=$(zcat "$p"_2.fastq.gz | awk -F'[=\" "]' '{print $4}' | head -1)
+  echo $length1
+  echo $length2
+    if [[ $length1 == 150 && $length2 == 150 ]]; then
+      echo "$p"
+        mv "$p"_1.fastq.gz "$p"_S1_L00"$lane"_R1_001.fastq.gz
+        mv "$p"_2.fastq.gz "$p"_S1_L00"$lane"_R2_001.fastq.gz
+    else 
+      echo "$p"
+      echo "Name of fastq file was not changed as reads from "$p"_1.fastq did not have length 8 and/or "$p"_2.fastq did not have length=28."
+    fi
+  fi
+done <../"$P"_SraAccList.txt
+
