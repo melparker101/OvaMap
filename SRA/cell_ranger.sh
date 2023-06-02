a3d11d28f4044f79d8f1e959f6d838f19ed4d818
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-06-02 13:48:18 +0100

    Update cell_ranger.sh

--- SRA/cell_ranger.sh
+++ SRA/cell_ranger.sh
@@ -12,8 +12,8 @@
 #SBATCH -p short
 #SBATCH -c 4
 #SBATCH -J cell_ranger
-#SBATCH -o logs/cell_ranger_%A_%a.out
-#SBATCH -e logs/cell_ranger_%A_%a.err
+#SBATCH -o logs/cell_ranger_project_%A_%a.out
+#SBATCH -e logs/cell_ranger_project_%A_%a.err
 #SBATCH -a 1-6
 
 #  Parallel environment settings 
@@ -31,40 +31,18 @@ echo "Username: "`whoami`
 echo "Started at: "`date` 
 echo "##########################################################"
 
+###################################
+# 1. Write a temporary script
+###################################
 
-# Set project variable
-if [[ ! $2 == run_script ]]; then
-  index=("PRJNA766716" "PRJNA836755" "PRJNA792835" "PRJNA754050" "PRJNA879764" "PRJNA849410")
-  PROJECT=${index[$SLURM_ARRAY_TASK_ID]}
-else
-  PROJECT=$1
-fi
+echo '#!/bin/bash
 
-# REF=$REF_GENOMES/homo_sapiens/10xgenomics
-
-# Load modules
-module load CellRanger/7.1.0
-
-# Index file containing a list of samples for a particular project
-INDEX="$PROJECT"/"$PROJECT"_SraAccList.txt
-
-# Number of array scripts to send off
-NSAMPLES=$(wc -l "$INDEX" | awk '{print $1}')  
-
-# Relaunch this script as an array for each sample in the project
-if [[ ! $2 == run_script ]]; then
-    echo "########################################################"
-  echo "Slurm Job ID: $SLURM_JOB_ID" 
-  echo "Run on host: "`hostname` 
-  echo "Operating system: "`uname -s` 
-  echo "Username: "`whoami` 
-  echo "Started at: "`date` 
-  echo "##########################################################"
-  echo ""
-  echo Resetting script...
-  # Carry over project number $1=PROJECT, set $2=run_script to prevent the script from resetting again
-  exec sbatch --array=1-"$NSAMPLES" "$0" $PROJECT run_script
-fi
+#SBATCH -A lindgren.prj
+#SBATCH -p short
+#SBATCH -c 4
+#SBATCH -J cell_ranger
+#SBATCH -o logs/cell_ranger_sample_%A_%a.out
+#SBATCH -e logs/cell_ranger_sample_%A_%a.err
 
 # Source .bashrc for the reference genome variable
 source ~/.bashrc
@@ -73,10 +51,11 @@ source ~/.bashrc
 module load CellRanger/7.1.0
 
 # Define variables
+PROJECT=$1
+INDEX="$PROJECT"/"$PROJECT"_SraAccList.txt
 REF=$REF_GENOMES/homo_sapiens/10xgenomics/refdata-gex-GRCh38-2020-A
 FASTQ=raw_reads
-SAMPLE=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' "$INDEX")
-
+SAMPLE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" "$INDEX")
 
 if [ !-d cellranger_count ]; then
   mkdir -p cellranger;
@@ -89,6 +68,33 @@ cellranger count --id run_count_"$PROJECT" \
                  --fastqs ../"$PROJECT"/"$FASTQ" \
                  --sample "$SAMPLE"
                  
+echo "###########################################################"
+echo "Finished at: "`date`
+echo "###########################################################"
+exit 0
+
+' > tmp_script.sh
+
+###################################
+# 2. Call the temporary script
+###################################
+
+# Set project number
+index=("PRJNA766716" "PRJNA836755" "PRJNA792835" "PRJNA754050" "PRJNA879764" "PRJNA849410")
+PROJECT=${index[$SLURM_ARRAY_TASK_ID]}
+
+# Index file containing a list of samples for a particular project
+INDEX="$PROJECT"/"$PROJECT"_SraAccList.txt
+
+# Number of array scripts to send off
+NSAMPLES=$(wc -l "$INDEX" | awk '{print $1}')  
+
+# Submit the temporary script file to Slurm with the project number as the first argument
+sbatch tmp_script.sh --array=1-"$NSAMPLES" $PROJECT
+
+# Remove the temporary script file
+rm tmp_script.sh
+
                  
 echo "###########################################################"
 echo "Finished at: "`date`
